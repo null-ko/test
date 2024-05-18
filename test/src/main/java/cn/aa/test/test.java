96d98bc21c0bbe3f87f1b968372e15d4c6d9c271
kingone1995 <124530521+kingone1995@users.noreply.github.com>
2024-05-19 00:23:40 +0800

    Initial commit

---
+++ test/src/main/java/cn/aa/test/test.java
@@ -0,0 +1,12 @@
+package cn.aa.test;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class test {
+    // 这是一个简单的方法示例，你可以根据需要进行修改
+    public static void Test_method(Context context) {
+        Toast.makeText(context, "这是第一次上传的测试", Toast.LENGTH_SHORT).show();
+    }
+
+}
